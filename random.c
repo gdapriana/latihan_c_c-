c35c7ee3d5ad624e1c09c94fa47056c3d75b4e05
gdapriana <S1540888@student.mcckc.edu>
2022-04-11 00:15:54 +0800

    test from ubuntu

--- random.c
+++ random.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-int main() { 
-    int a = 1;
-
-    while(a <= 100) {
-        printf("%d ", a);
-        a++;
-    }
+int main() {
+    printf("Hello World!");
+    return 0;
 }
