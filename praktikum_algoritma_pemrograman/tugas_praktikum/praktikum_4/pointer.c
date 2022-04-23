d4a83ce69db152c6028e4b3f18b33cfd13c002b7
gdapriana <S1540888@student.mcckc.edu>
2022-04-23 23:01:01 +0800

    update prak alpro 4

--- praktikum_algoritma_pemrograman/tugas_praktikum/praktikum_4/pointer.c
+++ praktikum_algoritma_pemrograman/tugas_praktikum/praktikum_4/pointer.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-void fungsiPertukaran(char nama){
+void tukar(char nama){
 	char inputPertukaran[20];
-	printf("Masukan input pertukaran : "); scanf("%s", &inputPertukaran);
+	printf("Masukan input pertukaran: "); scanf("%s", &inputPertukaran);
     printf("%s", inputPertukaran);
 }
 
-void fungsiPenambahan(char *nama) {
+void tambah(char *nama) {
 	char inputPenambahan[20];
 	
-	printf("Masukan input penambahan : "); scanf("%s",&inputPenambahan);
-    // preapare for error
+	printf("Masukan input penambahan: "); scanf("%s",&inputPenambahan);
 	printf("%s %s", nama, inputPenambahan);
 }
 
-void fungsiPerubahan(char nama){
-    char inputPerubahan;
+void ubah(char nama){
+    char inputPerubahan[20];
     
-	printf("Masukan input penambahan : "); scanf("%s",&inputPerubahan);    
-	if ( strlen(nama) == strlen(inputPerubahan) ) nama = inputPerubahan;
-	else printf("panjang string perubahan harus sama dengan panjang string input");
+	printf("Masukan input penambahan: "); scanf("%s",&inputPerubahan);    
+	if ( strlen(nama) == strlen(inputPerubahan) ) {
+		nama = inputPerubahan;
+	} else {
+		printf("panjang string perubahan harus sama dengan panjang string input");
+	}
 	printf("%s", nama);
 }
 
@@ -29,14 +31,18 @@ int main(){
     char nama[100];
     
     printf("Program Call by Reference dan by Value \n");
-    printf("1.Pertukaran + Call by Value \n2.Penambahan + Call by Reference \n3.fungsiPerubahan\n");
+    printf("1.Pertukaran + Call by Value \n2.Penambahan + Call by Reference \n3.ubah\n");
 	
 	printf("Masukan Pilihan : "); scanf("%d",&pilihan);
 	printf("Masukan nama : "); scanf("%s",&nama);
 	
-	if (pilihan == 1) fungsiPertukaran(nama);
-	else if (pilihan == 2) fungsiPenambahan(&nama);
-	else if (pilihan == 3 ) fungsiPerubahan(nama);
-	else printf("input salah");
-
+	if (pilihan == 1) {
+		tukar(nama);
+	} else if (pilihan == 2) {
+		tambah(&nama);
+	} else if (pilihan == 3 ) {
+		ubah(nama);
+	} else {
+		printf("input salah");
+	}
 }  
--- random.c
+++ random.c
@@ -1,25 +1,3 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-int main() { 
-    
-    int data[10] = {1,2,3,4,5,90,76,43,67,8};
-    int cari;
-    bool temu = false;
-
-    printf("Masukkan data yang ingin di cari: ");
-    scanf("%d", &cari);
-
-    for(int i = 0; i < sizeof(data); i++) {
-        if(data[i] == cari) {
-            temu = true;
-            break;
-        }
-    }
-
-    if(temu == true) {
-        printf("\nData Ditemukan!!\n");
-    } else {
-        printf("\nData Tidak Ditemukan!!\n");
-    }
+void hello(char name[]){
+    printf("Hello %s!\n", name);
 }
